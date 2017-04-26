6333eb59d65ff4ecfcd44dc599d97e3f6f236a07
jkjiwzx <jerapun.jiw@hotmail.com>
2017-04-26 22:02:29 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Home page</title>
+</head>
+<center><h1 style="color:#778899">Home Page</h1></center>
+<center><h2 style="color:#778899">งานวิชา 322-236 Web Application Programming</h2></center>
+<div>
+<center><h4 style="color:#778899"><b>จัดทำโดย :</b> นายเจรพัญ จิระชีวะนันท์ รหัสนักศึกษา 583020652-9<br>
+สาขาเทคโนโลยีสารสนเทศและการสื่อสาร ภาควิทยาการคอมพิวเตอร์<br>
+คณะวิทยาศาสตร์ มหาวิทยาลัยขอนแก่น <br>
+</h4></center>
+</div>
+</body>
+</html>
