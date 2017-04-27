650d43b1da9c2eab8d71e26163636db02cabe9df
jkjiwzx <jerapun.jiw@hotmail.com>
2017-04-27 15:45:38 +0700

    Add files via upload

--- main.php
+++ main.php
@@ -1,16 +1,92 @@
-<!doctype html>
-<html>
+﻿<html>
+
 <head>
-<meta charset="utf-8">
-<title>Home page</title>
-</head>
-<center><h1 style="color:#778899">Home Page</h1></center>
-<center><h2 style="color:#778899">งานวิชา 322-236 Web Application Programming</h2></center>
-<div>
-<center><h4 style="color:#778899"><b>จัดทำโดย :</b> นายเจรพัญ จิระชีวะนันท์ รหัสนักศึกษา 583020652-9<br>
-สาขาเทคโนโลยีสารสนเทศและการสื่อสาร ภาควิทยาการคอมพิวเตอร์<br>
-คณะวิทยาศาสตร์ มหาวิทยาลัยขอนแก่น <br>
-</h4></center>
-</div>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.css" type="text/css"> </head>
+
+<body class="bg-inverse">
+  <nav class="navbar navbar-expand-md navbar-light bg-faded">
+    <div class="container"> <a class="navbar-brand text-danger" href="main">PJ CAMERA&nbsp;</a> <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+               </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav ">
+          <li class="nav-item active"> <a class="nav-link" href="main">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="nikonpage1.html">Product</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">Register</a> </li>
+          <li class="nav-item"><a class="nav-link" href="#">log in</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-white text-uppercase">&nbsp;welcome to PJ Camera</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item text-white bg-danger"> <img class="d-block" src="Canon-EOS-1D-X-Mark-II_1.jpg" data-holder-rendered="true">
+                <div class="carousel-caption">
+                  <h3 class="text-right">Canon EOS-1D X Mark II</h3>
+                  <p class="text-right"> ประกัน ศูนย์ไทย ราคา 209,000.00 บาท</p>
+                </div>
+              </div>
+              <div class="carousel-item bg-warning active"> <img class="d-block img-fluid" src="Nikon-D750_1.jpg" data-holder-rendered="true">
+                <div class="carousel-caption">
+                  <h3 class="text-uppercase text-right">Nikon D750</h3>
+                  <p class="text-white text-right">ประกัน ศูนย์ไทย Body 59,990 บาท<br>ประกัน ศูนย์ไทย Body + 24-120 kit 79,990 บาท</p>
+                </div>
+              </div>
+              <div class="carousel-item bg-success"><img class="img-fluid" src="Sony-Alpha-a7II_1.jpg">
+                <div class="carousel-caption">
+                  <h2 class="text-right">Sony A7 ll</h2>
+                  <p class="text-right">
+                    <p class="text-right">ประกัน ศูนย์ไทย ราคา 56,990.00 บาท</p>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-inverse text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class=" pi-item">Contact</h1>
+          <p><b>ชั้น 2 ห้อง 14 A เซ็นทัลพลาซ่าขอนแก่น</b><br>TEL. 092-2700573</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 text-center bg-inverse">
+    <div class="container">
+      <div class="row">
+        <div class="col-4"> <a><i class="fa fa-5x fa-fw fa-facebook text-white"></i></a> </div>
+        <div class="col-4"> <a><i class="fa fa-5x fa-fw fa-twitter text-white"></i></a> </div>
+        <div class="col-4"> <a><i class="fa fa-5x fa-fw fa-instagram text-white"></i></a> </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+  <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
 </body>
+
 </html>
